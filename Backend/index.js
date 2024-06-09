380aaebe9cf2f589bd2f24b47ce095532a3e2871
Ankit <ankitchauhan21500@gmail.com>
2024-06-09 16:58:36 +0530

    successfully deployed 🎉

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://doodlequest.vercel.app",
         methods: ["POST", "GET"],
         credentials: true
     }

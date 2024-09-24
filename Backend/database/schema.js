47b9fdbc305e0cf6d0adc2f67320829504032898
Ankit <ankitchauhan21500@gmail.com>
2024-09-24 12:30:11 +0530

    deploy

--- Backend/database/schema.js
+++ Backend/database/schema.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 require('dotenv').config()
-mongoose.connect("mongodb://127.0.0.1:27017/Scribbles");
+// mongoose.connect("mongodb://127.0.0.1:27017/Scribbles");
+mongoose.connect("mongodb+srv://ankitchauhan21:<db_password>@doodle.ys5gt9k.mongodb.net/?retryWrites=true&w=majority&appName=doodle");
 
 
 const userSchema = new mongoose.Schema({

1d5025b8df98b2fd0bbb2a541f745a3b7de4f894
Ankit <ankitchauhan21500@gmail.com>
2024-12-18 15:49:53 +0530

    deploy on railway

--- Scribble_MERN/backendlink.js
+++ Scribble_MERN/backendlink.js
@@ -1,4 +1,4 @@
 
 // const backendLink = "http://localhost:3000";
-const backendLink = "https://doodleQuest.railway.internal";
+const backendLink = "https://doodlequest.railway.internal";
 export default backendLink; 

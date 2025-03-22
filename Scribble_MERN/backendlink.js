6cdcf7f6ff0b95463716e3edc558278f2cb56155
Ankit <ankitchauhan21500@gmail.com>
2025-03-22 14:42:06 +0530

    backend link updated

--- Scribble_MERN/backendlink.js
+++ Scribble_MERN/backendlink.js
@@ -1,4 +1,4 @@
 
-const backendLink = "http://localhost:3000";
-// const backendLink = "https://doodlequest.up.railway.app";
+// const backendLink = "http://localhost:3000";
+const backendLink = "https://doodlequest.up.railway.app";
 export default backendLink; 

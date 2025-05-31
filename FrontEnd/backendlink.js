58f506a3d33733b17d1b4b7b52ea3dd8646d1bf1
Ankit <ankitchauhan21500@gmail.com>
2025-05-31 20:03:43 +0530

    fixed backend link

--- FrontEnd/backendlink.js
+++ FrontEnd/backendlink.js
@@ -1,4 +1,5 @@
 
-const backendLink = "http://localhost:8000";
-// const backendLink  = "https://doodlequest-7.onrender.com";
+// const backendLink = "http://localhost:8000";
+const backendLink  = "https://doodlequest-7.onrender.com";
+// const backendLink  = "https://doodlequest.duckdns.org/";
 export default backendLink; 

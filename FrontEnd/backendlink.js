43dd6ef193ae89c8a7ab2cb12fcde03b47b080c6
Ankit Singh Chauhan <ankitchauhan21500@gmail.com>
2025-08-22 22:18:34 +0530

    tested for 300 concurrent users

--- FrontEnd/backendlink.js
+++ FrontEnd/backendlink.js
@@ -1,5 +1,4 @@
 
 // const backendLink = "http://localhost:8000";
-// const backendLink  = import.meta.env.VITE_BACKEND_LINK;
-const backendLink = "https://doodlebackend.me";
+const backendLink  = import.meta.env.VITE_BACKEND_LINK;
 export default backendLink; 

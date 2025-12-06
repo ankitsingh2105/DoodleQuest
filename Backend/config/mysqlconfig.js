3e22f6d384fdaf5d52e7eb4c24cb6f806bfeb721
Ankit Singh Chauhan <ankitchauhan21500@gmail.com>
2025-12-06 17:31:12 +0530

    new DB

--- Backend/config/mysqlconfig.js
+++ Backend/config/mysqlconfig.js
@@ -2,13 +2,17 @@ const mysql = require('mysql2/promise');
 
 async function main() {
   const connection = await mysql.createConnection({
-    host: 'database-1.cx8gi0quaphm.ap-south-1.rds.amazonaws.com',
+    host: 'database-4.cluster-c908o62io6j4.ap-south-1.rds.amazonaws.com',
     port: 3306,                                                  
-    user: 'ankit',                                               
+    user: 'admin',                                               
     password: 'newThechauhan1!',                                     
-    database: 'doodleDB'                                        
+    database: ''                                        
   });
   return connection;
 }
-
+// main().then(connection => {
+//   console.log('MySQL Connected Successfully', connection);
+// }).catch(err => {
+//   console.error('Error connecting to MySQL', err);
+// });
 module.exports = main;
